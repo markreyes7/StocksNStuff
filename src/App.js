964f3a8c8a93abf0e293a161bc12712cc8602311
Mark <mark.reyes7777@gmail.com>
2022-02-23 01:45:26 -0800

    Added chart.js data visualization from API data.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
 import TickerGroup from './Components/TickerGroup';
 import { useState, useEffect } from 'react';
-
+import Chart from './Components/Chart';
+import { Line } from 'react-chartjs-2';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
 
   const [symbolToRemove, setSymbolToRemove] = useState();
@@ -13,51 +15,61 @@ function App() {
   const [labelsToDisplay, setLabels] = useState([]);
   const [dataToDisplay, setData] = useState([]);
 
+
   useEffect(() => {
-    if(symbolToRemove === "undefined"){
+    console.log("symba is ")
+    console.log(symbolToRemove)
+  })
+
+  useEffect(() => {
+    if (symbolToRemove === "undefined") {
       return
     }
-    else{
-    let labels =[];
-    let information=[]
-    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbolToRemove}&apikey=MWDL1LRHL23LC64V`).then(res => res.json())
-      .then((data) => {
-        console.log("from the main APP")
-        var info = data["Time Series (Daily)"];
-        Object.keys(info).slice(1,5).map((keys, index) => labels.push(keys));
-        setLabels(labels)
-        console.log("labels")
-        console.log(labels)
+    else {
+      let labels = [];
+      let information = []
+      fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbolToRemove}&apikey=APIKEY`).then(res => res.json())
+        .then((data) => {
+          console.log("from the main APP")
+          var info = data["Time Series (Daily)"];
+          Object.keys(info).slice(1, 5).map((keys, index) => labels.push(keys));
+          setLabels(labels)
+          console.log("labels")
+          console.log(labels)
 
-        for(var i=0; i <labels.length;i++){
-          information.push(data["Time Series (Daily)"][labels[i]["2. high"]])
-        }
-        setData(information)
+          for (var i = 0; i < labels.length; i++) {
+            console.log("from the inside loop")
+            console.log(labels[i])
+            console.log(data["Time Series (Daily)"][labels[i]]["2. high"])
+            information.push(data["Time Series (Daily)"][labels[i]]["2. high"])
+          }
+          console.log("the info here is ")
+          console.log(information)
+          setData(information)
 
-      })
-  }}, [symbolToRemove])
+        })
 
-  // useEffect(() => {
-  //   if(symbolToRemove === "undefined"){
-  //     return
-  //   }
-  //   else{
-  //   let information = []
-  //   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbolToRemove}&apikey=MWDL1LRHL23LC64V`).then(res => res.json())
-  //   .then((results) => {
-  //     for(var i =0; i < labelsToDisplay.length;i++){
-  //       console.log("inside loop")
-  //       console.log(labelsToDisplay[i])
-  //       information.push(results["Time Series (Daily)"][labelsToDisplay[i]]["2. high"])
-  //       setData(information)
-  //       console.log("info here")
-  //       console.log(information)
-  //     }
-      
-  //   })
-  // }},[symbolToRemove, labelsToDisplay])
- 
+    }
   
+  }, [symbolToRemove])
+
+  let obj = {
+    labels: labelsToDisplay,
+    datasets: [
+      {
+        label: "Prices in USD",
+        data: dataToDisplay,
+        borderColor:'rgb(255, 99, 132)',
+        backgroundColor: [
+          "#ffbb11",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ]
+      }
+    ]
+  }
 
   return (
     <div>
@@ -72,7 +84,7 @@ function App() {
             <TickerGroup setSymbolToRemove={setSymbolToRemove} symbolToRemove={symbolToRemove} />
           </Col>
           <Col lg={10} xl={10}>
-            {(symbolToRemove === "undefined") ? (<p style={{ color: "red" }}>LOADING</p>) : (<p style={{ color: "white" }}>{symbolToRemove}</p>)}
+            {(typeof symbolToRemove !== "undefined") ? (<Line data={obj}></Line>) : (<p>HI</p>)}
           </Col>
         </Row>
       </Container>
